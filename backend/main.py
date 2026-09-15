e288987759b7e8f2cc406db1a8ea9e6487670bd8
Sherika Fayson <137829671+Sfayson1@users.noreply.github.com>
2026-09-15 17:53:57 -0400

    refactor: remove unused users router

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from database import init_db
-from routers import users, posts, auth, prompts
+from routers import posts, auth, prompts
 
 load_dotenv()
 
@@ -31,7 +31,6 @@ app.add_middleware(
 
 app.include_router(prompts.router, prefix="/api/prompts", tags=["Prompts"])
 app.include_router(posts.router, prefix="/api/posts", tags=["Posts"])
-app.include_router(users.router, prefix="/api/users", tags=["Users"])
 app.include_router(auth.router, prefix="/api/auth", tags=["Auth"])
 
 @app.get("/test-token")
